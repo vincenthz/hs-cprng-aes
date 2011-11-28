ce3afee63a262f30ffba0204ab3812b25b5c33ec
Vincent Hanquez <vincent@snarc.org>
2011-11-28 21:02:48 +0000

    optimise data structures
    - unpack data completely since it's going to be
      fully changed at each iteration.
    - optimise the case where genRandomBytes get only 1 chunk.
    - use a word128 as IV which should increase speed for xoring.

--- Crypto/Random/AESCtr.hs
+++ Crypto/Random/AESCtr.hs
@@ -34,10 +34,13 @@ import Data.Word
 import Data.Bits (xor, (.&.))
 import Data.Serialize
 
-data Word128 = Word128 !Word64 !Word64
+data Word128 = Word128 {-# UNPACK #-} !Word64 {-# UNPACK #-} !Word64
 
 {-| An opaque object containing an AES CPRNG -}
-data AESRNG = RNG !ByteString !Word128 !AES.Key
+data AESRNG = RNG
+	{-# UNPACK #-} !Word128 -- ^ IV
+	{-# UNPACK #-} !Word128 -- ^ Counter
+	{-# UNPACK #-} !AES.Key -- ^ Key
 
 instance Show AESRNG where
 	show _ = "aesrng[..]"
@@ -47,6 +50,9 @@ put128 (Word128 a b) = runPut (putWord64host a >> putWord64host b)
 
 get128 :: ByteString -> Word128
 get128 = either (\_ -> Word128 0 0) id . runGet (getWord64host >>= \a -> (getWord64host >>= \b -> return $ Word128 a b))
+
+xor128 :: Word128 -> Word128 -> Word128
+xor128 (Word128 a1 b1) (Word128 a2 b2) = Word128 (a1 `xor` a2) (b1 `xor` b2)
 
 add1 :: Word128 -> Word128
 add1 (Word128 a b) = if b == 0xffffffffffffffff then Word128 (a+1) 0 else Word128 a (b+1)
@@ -66,22 +72,19 @@ makeParams b = (key, cnt, iv)
 make :: B.ByteString -> Either GenError AESRNG
 make b
 	| B.length b < 64 = Left NotEnoughEntropy
-	| otherwise       = Right $ RNG iv (get128 cnt) key
+	| otherwise       = Right $ RNG (get128 iv) (get128 cnt) key
 		where
 			(key, cnt, iv) = makeParams b
 
 chunkSize :: Int
 chunkSize = 16
 
-bxor :: ByteString -> ByteString -> ByteString
-bxor a b = B.pack $ B.zipWith xor a b
-
-nextChunk :: AESRNG -> (ByteString, AESRNG)
-nextChunk (RNG iv counter key) = (chunk, newrng)
+genNextChunk :: AESRNG -> (ByteString, AESRNG)
+genNextChunk (RNG iv counter key) = (chunk, newrng)
 	where
-		newrng = RNG chunk (add1 counter) key
+		newrng = RNG (get128 chunk) (add1 counter) key
 		chunk  = AES.encrypt key bytes
-		bytes  = iv `bxor` (put128 counter)
+		bytes  = put128 (iv `xor128` counter)
 
 -- | Initialize a new AES RNG using the system entropy.
 makeSystem :: IO AESRNG
@@ -94,13 +97,13 @@ makeSystem = ofRight . make <$> getEntropy 64
 -- it generate randomness by block of 16 bytes, but will truncate
 -- to the number of bytes required, and lose the truncated bytes.
 genRandomBytes :: AESRNG -> Int -> (ByteString, AESRNG)
-genRandomBytes rng n =
-	let list = helper rng n in
-	(B.concat $ map fst list, snd $ last list)
+genRandomBytes rng 16 = genNextChunk rng
+genRandomBytes rng n  = (B.concat $ map fst list, snd $ last list)
 	where
+		list = helper rng n
 		helper _ 0 = []
 		helper g i =
-			let (b, g') = nextChunk g in
+			let (b, g') = genNextChunk g in
 			if chunkSize >= i
 				then [ (B.take i b, g') ]
 				else (b, g') : helper g' (i-chunkSize)
@@ -111,14 +114,14 @@ instance CryptoRandomGen AESRNG where
 	genBytes len rng = Right $ genRandomBytes rng len
 	reseed b rng@(RNG _ cnt1 _)
 		| B.length b < 64 = Left NotEnoughEntropy
-		| otherwise       = Right $ RNG (r16 `bxor` iv2) (get128 (put128 cnt1 `bxor` cnt2)) key2
+		| otherwise       = Right $ RNG (get128 r16 `xor128` get128 iv2) (cnt1 `xor128` get128 cnt2) key2
 			where
-				(r16, _)          = nextChunk rng
+				(r16, _)          = genNextChunk rng
 				(key2, cnt2, iv2) = makeParams b
 
 instance RandomGen AESRNG where
 	next rng =
-		let (bs, rng') = nextChunk rng in
+		let (bs, rng') = genNextChunk rng in
 		let (Word128 a _) = get128 bs in
 		let n = fromIntegral (a .&. 0x7fffffff) in
 		(n, rng')
